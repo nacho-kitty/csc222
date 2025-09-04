eb40617826b09046abda557ea72f2898a8f646b4
funneralplanner <plsdontatm3@gmail.com>
2025-09-03 20:45:26 -0400

    added 2 of ten_expressions. need to do more still

---
+++ ten_mathematical_expressions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //1
+    int a (1);
+    int b (2);
+
+    cout << "the sum of " << a << " and " << b << " is : " << a * b << endl;
+
+    //2
+    int c (3);
+    int d (20);
+
+    cout << "i can add, but i can also subtract! here's " << c << 
+        " minus " << d << ": " << c - d << endl;
+
+    return 0;
+
+}
