47051412ca01cbcb3b254c1d59d41e15cc5fecdd
funneralplanner <plsdontatm3@gmail.com>
2025-09-08 16:38:40 -0400

    finished the compare thingygit add *git add *

--- compare.cpp
+++ compare.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int compare(int a, int b){
-    if (a >= b){
+    if (a > b){
         cout << "a is greater than b" << endl;
-    } else if (a <= b){
+    } else if (a < b){
         cout << "a is less than b" << endl;
     } else {
         cout << " a is equal to b" << endl;
@@ -19,9 +19,9 @@ int main(){
     int a;
     int b;
 
-    cin = a;
-    cin = b;
-    compare();
+    cin >> a;
+    cin >> b;
+    compare(a,b);
 
     return 0;
 }
